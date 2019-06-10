637df29fbd649c9c13f70a5c5e974ff7554adeed
Jacob Solomon <j.solomon@knights.ucf.edu>
2019-06-09 22:43:33 -0400

    Add test tracks

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,13 +1,14 @@
 import L from 'leaflet';
 import '../../LeafletPlayback';
+import 'leaflet-bing-layer';
 
 export default class Map {
     constructor () {
 
     }
-    
     render () {
-      var map = L.map('map').setView([28.538336, -81.379234], 13);
+      var map = L.map('map').setView([28.538336, -81.379234], 4);
+      // L.tileLayer.bing('AuhiCJHlGzhg93IqUH_oCpl_-ZUrIE6SPftlyGYUvr9Amx5nzA-WqGcPquyFZl4L').addTo(map);
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
         maxZoom: 18,
@@ -15,11 +16,25 @@ export default class Map {
       var playbackOptions = {
         playControl: true,
         dateControl: true,
-        sliderControl: true
+        sliderControl: true,
+        fadeMarkersWhenStale: true,
+        speed: 10000,
       };
+      L.geoJSON({
+        'type': 'Feature',
+        'geometry': {
+          'type': 'LineString',
+          'coordinates': demoTracks[0]['geometry']['coordinates']
+        },
+        'properties': {}
+      }).addTo(map);
 
       // Initialize playback
       var playback = new L.Playback(map, demoTracks, null, playbackOptions);
+      console.log(playback);
+
+      
+
 
     }
 
